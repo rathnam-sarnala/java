eb16bf1cf40fef1529afea9e9d502a0df2c84ec3
rathnam-sarnala <151710876+rathnam-sarnala@users.noreply.github.com>
2024-10-17 11:35:53 +0530

    Create  Addition2.java

---
+++ Addition2.java
@@ -0,0 +1,13 @@
+
+import java.io.*;
+
+class Addition2   
+{					    
+	public static void main(String args[])
+	{
+		int x=Integer.parseInt(args[0]);
+		int y=Integer.parseInt(args[1]);
+		int z=x+y;
+		System.out.println("Sum of "+x+" and "+y+" is "+z);
+	}
+}
