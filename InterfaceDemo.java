dafd339155bd4afd9c948d0d679cd9376b81007d
rathnam-sarnala <151710876+rathnam-sarnala@users.noreply.github.com>
2024-10-19 12:29:22 +0530

    Create  InterfaceDemo.java

---
+++ InterfaceDemo.java
@@ -0,0 +1,29 @@
+//To demonstrate Interface and Functional Interface
+interface Drawable
+{  
+	void draw();  
+}  
+ 
+class Rectangle implements Drawable
+{  
+	public void draw()
+	{
+		System.out.println("drawing rectangle");
+	}  
+}  
+class Circle implements Drawable
+{  
+	public void draw()
+	{
+		System.out.println("drawing circle");
+	}  
+}  
+ 
+class InterfaceDemo
+{  
+	public static void main(String args[])
+	{  
+		Drawable d=new Circle();   
+		d.draw();  
+	}
+}  
